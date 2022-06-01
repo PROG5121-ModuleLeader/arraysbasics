fc2eebfc29a09055c6709255c1a392361bf57451
rece212 <rwanvig@iie.ac.za>
2022-06-01 11:33:49 +0200

    Added new file

---
+++ test/prjarraysinclasses/PrjArraysInClassesTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjarraysinclasses;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rwanv
+ */
+public class PrjArraysInClassesTest {
+    
+    public PrjArraysInClassesTest() {
+    }
+
+    /**
+     * Test of main method, of class PrjArraysInClasses.
+     */
+    @Test
+    public void testMain() {
+    }
+
+    /**
+     * Test of StudentOption1 method, of class PrjArraysInClasses.
+     */
+    @Test
+    public void testStudentOption1() {
+    }
+    
+}
